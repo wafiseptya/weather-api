c2c9a4f1186647b29e77f1c4468d000c2b477704
wafiseptya <septya.wafi@gmail.com>
2019-05-23 08:42:36 +0700

    Updates

---
+++ precache-manifest.d155b486f8d7f4ea17c90f0c1d7fb4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61bb8b45aac3c02d62ec45a8eebd535",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "09e2b9aac6ed39c3d992",
+    "url": "/weather-api/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/weather-api/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "09e2b9aac6ed39c3d992",
+    "url": "/weather-api/static/js/main.42e8dc23.chunk.js"
+  },
+  {
+    "revision": "a104ea585fd9135dd611",
+    "url": "/weather-api/static/js/runtime~main.4827966c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
